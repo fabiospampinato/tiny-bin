abb5c9bb43af274cb6ed6ed1ab0f210638bd4109
Fabio Spampinato <spampinabio@gmail.com>
2026-01-26 01:16:01 +0000

    Tests: ensuring the output has no colors

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ describe ( 'Tiny Bin', it => {
 
   it ( 'can return the version', t => {
 
-    const output = execSync ( './resources/demo_gitman.js --version', { encoding: 'utf8' } ).trim ();
+    const output = execSync ( './resources/demo_gitman.js --version --no-colors', { encoding: 'utf8' } ).trim ();
 
     t.is ( output.trim (), '1.2.3' );
 
@@ -20,7 +20,7 @@ describe ( 'Tiny Bin', it => {
 
   it ( 'can return the bin help page', t => {
 
-    const output = execSync ( './resources/demo_bump.js --help', { encoding: 'utf8' } ).trim ();
+    const output = execSync ( './resources/demo_bump.js --help --no-colors', { encoding: 'utf8' } ).trim ();
 
     t.true ( output.includes ( 'bump' ) );
     t.true ( output.includes ( 'USAGE' ) );
@@ -36,7 +36,7 @@ describe ( 'Tiny Bin', it => {
 
   it ( 'can return the command help page', t => {
 
-    const output = execSync ( './resources/demo_bump.js release --help', { encoding: 'utf8' } ).trim ();
+    const output = execSync ( './resources/demo_bump.js release --help --no-colors', { encoding: 'utf8' } ).trim ();
 
     t.true ( output.includes ( 'bump release' ) );
     t.true ( output.includes ( 'USAGE' ) );
@@ -52,11 +52,11 @@ describe ( 'Tiny Bin', it => {
 
   it ( 'can execute a command', t => {
 
-    const output1 = execSync ( './resources/demo_gitman.js user repo', { encoding: 'utf8' } ).trim ();
+    const output1 = execSync ( './resources/demo_gitman.js user repo --no-colors', { encoding: 'utf8' } ).trim ();
 
     t.true ( output1.includes ( 'Default command executed' ) );
 
-    const output2 = execSync ( './resources/demo_gitman.js clone user repo', { encoding: 'utf8' } ).trim ();
+    const output2 = execSync ( './resources/demo_gitman.js clone user repo --no-colors', { encoding: 'utf8' } ).trim ();
 
     t.true ( output2.includes ( 'Clone command executed' ) );
 
